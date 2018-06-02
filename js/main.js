16d8415bb77b1ef608805aba45aeb36c71cd624b
shivansh007 <shivanshnlw@gmail.com>
2018-06-02 17:29:49 +0530

    Add jquery offlive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
 var JS = JS || {};
 
 JS.scripts = function(){
-  this.initialize();
+	this.initialize();
 }
 
 JS.scripts.prototype = {
-  initialize:function(){
-  }
+	initialize:function(){
+		this.bindEvents();
+	},
+	bindEvents: function(){
+		$('.js-spanize').html(function (i, el) {
+        //spanizeLetters.joinChars();
+        var spanizer = $.trim(el).split("");
+        return '<span>' + spanizer.join('</span><span>') + '</span>';
+      });
+	},
 }
+
+
+
+// (function($) {
+// 	var s,
+// 	spanizeLetters = {
+// 		settings: {
+// 			letters: $('.js-spanize'),
+// 		},
+// 		init: function() {
+// 			s = this.settings;
+// 			this.bindEvents();
+// 		},
+// 		bindEvents: function(){
+// 			s.letters.html(function (i, el) {
+//         //spanizeLetters.joinChars();
+//         var spanizer = $.trim(el).split("");
+//         return '<span>' + spanizer.join('</span><span>') + '</span>';
+//       });
+// 		},
+// 	};
+// 	spanizeLetters.init();
+// })(jQuery);
